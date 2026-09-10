cfcdda75972456925ae70d108251bbd4bb95e454
Sorin Vinatoru <sorinv@users.noreply.github.com>
2026-09-10 14:46:29 +0300

    Update import path for customSendgridPlugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { formsPlugin } from "@emdash-cms/plugin-forms";
 import webhookNotifier from "@emdash-cms/plugin-webhook-notifier";
 import { defineConfig, fontProviders } from "astro/config";
 import emdash from "emdash/astro";
-import { customSendgridPlugin } from './sendgrid.js';
+import { customSendgridPlugin } from './src/plugins/sendgrid.js';
 
 
 const emdashConfig = {
