0016e918e1053c798c961ade6b66aac87fae261d
Sorin Vinatoru <sorinv@users.noreply.github.com>
2026-09-10 13:41:18 +0300

    Replace brevoPlugin with customBrevoPlugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { formsPlugin } from "@emdash-cms/plugin-forms";
 import webhookNotifier from "@emdash-cms/plugin-webhook-notifier";
 import { defineConfig, fontProviders } from "astro/config";
 import emdash from "emdash/astro";
-import { brevoPlugin } from "emdash-plugin-brevo";
+import { customBrevoPlugin } from "./brevo-plugin.js"
 
 export default defineConfig({
 	output: "server",
@@ -24,7 +24,7 @@ export default defineConfig({
 			plugins: [
 				formsPlugin(), 
 				webhookNotifier, 
-				brevoPlugin()
+				customBrevoPlugin()
 			],
 			sandboxed: [],
 			sandboxRunner: sandbox(),
